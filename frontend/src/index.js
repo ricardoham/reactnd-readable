4a7c3e34be313d57ae7045e2d5b1a89bb527d9a6
Hamanor <rdomanoel@gmail.com>
2018-11-07 22:54:16 -0200

    feat: sass arch on project

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store/config-store';
 import App from './components/App';
+import './sass/main.scss';
 
 const store = configureStore();
 
