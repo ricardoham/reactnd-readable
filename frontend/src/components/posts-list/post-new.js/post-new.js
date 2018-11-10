3a7d2ac1f1d655db81f424c313745c9a2daf277e
Hamanor <rdomanoel@gmail.com>
2018-11-10 12:47:01 -0200

    wip: form validation

--- frontend/src/components/posts-list/post-new.js/post-new.js
+++ frontend/src/components/posts-list/post-new.js/post-new.js
@@ -1,44 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 class PostNew extends React.PureComponent {
-  handleSubmit = () => {
-    const { onSubmit } = this.props;
-    onSubmit();
-  }
 
   render() {
+    const {
+      values,
+      errors,
+      dirty,
+      status,
+      touched,
+      handleBlur,
+      handleChange,
+      handleSubmit,
+      isSubmitting,
+    } = this.props;
+
     return (
       <Formik>
-
-      {/* {props => {
-        values
-      }} */}
-
-        <form onSubmit={this.handleSubmit}>
-          <label htmlFor="title">
-            Title:
-            <input name="title" type="title" />
-          </label>
-          <label htmlFor="author">
-            Author:
-            <input />
-          </label>
-          <label htmlFor="post">
-            Post:
-            <textarea />
-          </label>
-          <button type="button">Submit Post!</button>
-          <button type="button">Cancel</button>
+        <form onSubmit={handleSubmit}>
+          <div className="post-new">
+            <label htmlFor="title">
+              Title:
+              <input
+                name="title"
+                type="title"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                // value={values.title}
+              />
+            </label>
+            <label htmlFor="author">
+              Author:
+              <input
+                name="author"
+                type="author"
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+            <label htmlFor="body">
+              Post:
+              <textarea
+                name="body"
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </label>
+          </div>
+          <button
+            type="button"
+            disabled={!dirty || isSubmitting}
+          >
+            Submit Post!
+          </button>
+          <button type="button" disabled={isSubmitting}>Cancel</button>
         </form>
       </Formik>
     );
   }
 }
 
-PostNew.protoType = {
-  onSubmit: PropTypes.func.isRequired,
-};
+export default formikValidations({
+  mapPropsToValues
+})
 
-export default PostNew;
+// export default PostNew;
