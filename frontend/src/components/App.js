5fd04558b8367562ae67f894689b32fdba946174
Hamanor <rdomanoel@gmail.com>
2018-12-04 00:46:18 -0200

    new components, and new layout

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Routes from '../routes/routes';
-import Navbar from './navbar/navbar';
+import Navbar from './template/navbar/navbar';
+import Header from './template/header/header';
+import Footer from './template/footer/footer';
 // import PostsCategoriesContainer from '../components/posts-categories/posts-categories-container';
 // import PostEditContainer from '../components/posts-list/post-edit/post-edit-container';
 
 const App = () => (
   <div>
+    <Header />
     <Navbar />
     <Routes />
+    <Footer />
   </div>
 );
 
