e2ef265ad1b9991c0cea630d2201085857cf5a07
Hamanor <rdomanoel@gmail.com>
2018-12-14 02:27:46 -0200

    resolving the error module not found

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCheckSquare } from '@fortawesome/free-solid-svg-icons';
 import Routes from '../routes/routes';
 import Navbar from './template/navbar/navbar';
 import Footer from './template/footer/footer';
-
-library.add(faCheckSquare);
 
 const App = () => (
   <div>
     <Navbar />
     <div className="container">
-
       <Routes />
-
     </div>
     <Footer />
   </div>
